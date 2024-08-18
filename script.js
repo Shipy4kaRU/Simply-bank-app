6e4adef62fa4197419e1a09ce69469a99a2186d8
Shipy4kaRU <moloko2004goda@gmail.com>
2024-08-18 11:32:45 +0400

    update sort()

--- script.js
+++ script.js
@@ -285,7 +285,6 @@ btnLoan.addEventListener('click', askLoan);
 
 btnSort.addEventListener('click', function () {
   areSorted >= 1 ? (areSorted = -1) : areSorted++;
-  console.log(areSorted);
   switch (areSorted) {
     case 1:
       btnSort.innerHTML = '&downarrow; СОРТИРОВКА';
